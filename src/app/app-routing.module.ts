502fbb0d141714555283ad94712e734639be976b
Wagner Costa <wg.o.costa@gmail.com>
2020-10-14 08:39:36 -0300

    table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponenteListComponent } from './componente/componente-list/componente-list.component';
 
-const routes: Routes = [{
+const routes: Routes = [
+  {
+    path: '',
+    component: ComponenteListComponent
+  },
+  {
   path: 'componente',
   loadChildren: () => import('./componente/componente.module').then(m => m.ComponenteModule)
 }];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { RouterModule } from '@angular/router';
 
+
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
